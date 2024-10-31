9cc7fe6878cb1a9eb4edde5717ee23b74888c4e3
김경환 <khwankim@smilegate.com>
2024-10-31 05:34:23 +0000

    Add qdrant quickstart

---
+++ qdrant/quickstart/README.md
@@ -0,0 +1,21 @@
+# Qdrant quickstart
+
+## Run
+- Run qdrant with docker
+```
+docker run --rm -p 6333:6333 -p 6334:6334 \
+	-v $(pwd)/qdrant_storage:/qdrant/storage:z \
+	qdrant/qdrant
+```
+    - Check http://localhost:6333/dashboard
+- Create collection and add vectors
+    - Collection 생성 및 더미 vector 삽입
+```
+python create.py
+```
+
+- Run query
+
+```
+python query.py
+```
---
+++ qdrant/quickstart/create.py
@@ -0,0 +1,25 @@
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
+from qdrant_client.models import PointStruct
+
+client = QdrantClient(url="http://localhost:6333")
+
+client.create_collection(
+    collection_name="test_collection",
+    vectors_config=VectorParams(size=4, distance=Distance.DOT),
+)
+
+operation_info = client.upsert(
+    collection_name="test_collection",
+    wait=True,
+    points=[
+        PointStruct(id=1, vector=[0.05, 0.61, 0.76, 0.74], payload={"city": "Berlin"}),
+        PointStruct(id=2, vector=[0.19, 0.81, 0.75, 0.11], payload={"city": "London"}),
+        PointStruct(id=3, vector=[0.36, 0.55, 0.47, 0.94], payload={"city": "Moscow"}),
+        PointStruct(id=4, vector=[0.18, 0.01, 0.85, 0.80], payload={"city": "New York"}),
+        PointStruct(id=5, vector=[0.24, 0.18, 0.22, 0.44], payload={"city": "Beijing"}),
+        PointStruct(id=6, vector=[0.35, 0.08, 0.11, 0.44], payload={"city": "Mumbai"}),
+    ],
+)
+
+print(operation_info)
